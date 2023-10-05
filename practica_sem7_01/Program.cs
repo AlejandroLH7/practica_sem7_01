d38a431e9e0885be75cf646a8959f7354326226f
audio <audio@LAPTOP-A2DKNN9M>
2023-10-04 22:34:23 -0500

    Agregar archivos de proyecto.

---
+++ practica_sem7_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_sem7_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int N = int.Parse(Console.ReadLine());
+            float result = 0f;
+            float suma = 0f;
+            for (int i = 1; i <= N; i++)
+            {
+                result = (16f - i) / (3f * i);
+                Console.WriteLine("resultado : " + result);
+                suma += result;
+            }
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
